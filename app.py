b938a7d7a789dd57f71c980ea9eee573d3edf3b9
Farhad <farhadk07@gmail.com>
2022-11-05 19:00:14 +0000

    app update 2

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is my First test Project for CI/CD Happy Learning...'
+    return 'This is my First test Project for CI/CD Happy Learning...!'
